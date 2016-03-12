8667426ad5a4e9738beaf180a555d54dc03bd9b8
Pramith Prakash <pramithprakash@gmail.com>
2016-03-12 23:03:45 +0530

    changes

--- tketz/library/js/app.js
+++ tketz/library/js/app.js
@@ -1,3 +1,12 @@
 function login(){
   $('header').addClass('expanded');
 }
+$('[role="menu-open"]').on('click',function(){
+  $('body,.menu-offcanvas').addClass('open');
+});
+$('[role="menu-close"]').on('click',function(){
+  $('.menu-offcanvas').removeClass('open');
+  setTimeout(function(){
+    $('body').removeClass('open');
+  },500);  
+});
