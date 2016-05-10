90aa43e32cf18edf8b558493190c8370a3689e37
Pramith Prakash <pramithprakash@gmail.com>
2016-05-10 21:26:24 +0530

    added

--- pramith/uncompressed/js/controllers/data-controller.js
+++ pramith/uncompressed/js/controllers/data-controller.js
@@ -18,6 +18,24 @@ define(
         /* Controllers */
         return angular.module( 'Data.controllers', [] )
 
+        .filter( 'trust', [
+
+            '$filter',
+            '$sce',
+
+            function(
+
+                $filter,
+                $sce
+            ) {
+
+                return function( string ) {
+
+                    return $sce.trustAsHtml( string );
+                };
+            }
+        ])
+
         .controller( 'dataController', [
 
             '$scope',
@@ -37,6 +55,48 @@ define(
                 var init = function () {
 
                     var url = ( 'http://localhost/pramith/working/pramithprakash.github.io/pramith/data/data.json' );
+
+                    $scope.miliseconds = "164630000";
+
+                    var counter
+                        , counter1
+                        , ct1
+                        , t1 = new Date(2009, 10, 3, 0, 0, 0, 0)
+                        , t2 = new Date()
+                        , dif = t1.getTime() - t2.getTime()
+                        ;
+                    
+                    ct1 = parseInt(Math.abs(dif)/ 1000 - 1000);
+
+                    function timerLoop() {
+                        
+                        t2 = new Date();
+                        dif = t1.getTime() - t2.getTime();
+
+                        var Seconds_from_T1_to_T2 = dif / 1000;
+                        var Seconds_Between_Dates = Math.abs(Seconds_from_T1_to_T2);
+
+                        $scope.miliseconds = parseInt(Seconds_Between_Dates);
+                        $scope.$apply();
+                        
+                    }
+                    function scopeApply() {
+
+                        if ( !$scope.$$phase ) { $scope.$apply(); }
+                    }
+                    function counterLoop() {
+
+                        ct1 = ct1 + 1 ;
+                        
+                        $scope.miliseconds = parseInt(ct1);
+                        scopeApply();
+                        if (ct1 > parseInt(Math.abs(dif)/ 1000) ) {
+                            
+                          counter = setInterval( timerLoop, 1000 );
+                          clearInterval( counter1 );		
+                        }	
+                    }
+                    counter1 = setInterval( counterLoop, 1 );
 
                     $http.get( url )
                         .success( function ( data, status, headers, config ) {
