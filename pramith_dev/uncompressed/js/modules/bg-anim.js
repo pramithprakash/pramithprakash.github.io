42d7897c1c45342a6c52cf52e382df4d447ee428
Pramith Prakash <pprakash2@sapient.com>
2016-10-28 14:27:03 +0530

    update

--- pramith_dev/uncompressed/js/modules/bg-anim.js
+++ pramith_dev/uncompressed/js/modules/bg-anim.js
@@ -22,10 +22,10 @@
 
         // create points
         points = [];
-        for(var x = 0; x < width; x = x + width/50) {
-            for(var y = 0; y < height; y = y + height/50) {
-                var px = x + Math.random()*width/50;
-                var py = y + Math.random()*height/50;
+        for(var x = 0; x < width; x = x + width/20) {
+            for(var y = 0; y < height; y = y + height/20) {
+                var px = x + Math.random()*width/20;
+                var py = y + Math.random()*height/20;
                 var p = {x: px, originX: px, y: py, originY: py };
                 points.push(p);
             }
