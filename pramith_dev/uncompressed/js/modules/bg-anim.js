4c3bdc8d070aab633ad153d227e387edfa89dd26
Pramith Prakash <pprakash2@sapient.com>
2016-10-28 13:08:19 +0530

    FIX - dev

--- pramith_dev/uncompressed/js/modules/bg-anim.js
+++ pramith_dev/uncompressed/js/modules/bg-anim.js
@@ -5,7 +5,7 @@
     // Main
     initHeader();
     initAnimation();
-    addListeners();
+    //addListeners();
 
     function initHeader() {
         width = window.innerWidth;
@@ -22,10 +22,10 @@
 
         // create points
         points = [];
-        for(var x = 0; x < width; x = x + width/20) {
-            for(var y = 0; y < height; y = y + height/20) {
-                var px = x + Math.random()*width/20;
-                var py = y + Math.random()*height/20;
+        for(var x = 0; x < width; x = x + width/50) {
+            for(var y = 0; y < height; y = y + height/50) {
+                var px = x + Math.random()*width/50;
+                var py = y + Math.random()*height/50;
                 var p = {x: px, originX: px, y: py, originY: py };
                 points.push(p);
             }
@@ -153,7 +153,7 @@
             ctx.beginPath();
             ctx.moveTo(p.x, p.y);
             ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
+            ctx.strokeStyle = 'rgba(255,81,47,'+ p.active+')';
             ctx.stroke();
         }
     }
@@ -172,7 +172,7 @@
             if(!_this.active) return;
             ctx.beginPath();
             ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
+            ctx.fillStyle = 'rgba(255,81,47,'+ _this.active+')';
             ctx.fill();
         };
     }
