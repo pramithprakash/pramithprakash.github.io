fdcfa5b0b9e96ca657e08eb098b0312dea20e0b7
Pramith Prakash <pprakash2@sapient.com>
2021-05-16 13:04:26 +0530

    Create Interview Q

---
+++ interviewQuestions/src/index.js
@@ -0,0 +1,78 @@
+import "./styles.css";
+import $ from "jquery";
+
+var buttonClickCount = 0;
+
+const buttonClickCounterEl = $("#button_click_counter");
+const increaseBtn = $("#increase_button");
+
+increaseBtn.click(() => {
+  ++buttonClickCount;
+  buttonClickCounterEl.text(buttonClickCount);
+});
+
+const start = $("#startArray");
+const stop = $("#stopArray");
+var interval;
+var arr = ["a", "b", "c"];
+
+start.click(() => {
+  interval = setInterval(myTimer, 1000);
+});
+
+function myTimer() {
+  $("#array").append(arr + "_");
+  arr.unshift(arr.pop());
+}
+
+stop.click(() => {
+  clearInterval(interval);
+});
+
+/*
+let group = {
+  title: "Our Players",
+  players: ["John", "Pete", "Alice"],
+
+  showList() {
+    this.players.forEach(function (player) {
+      alert(this.title + ": " + player);
+    });
+  }
+};
+
+group.showList();
+*/
+
+/*
+let group = {
+  title: "Our Players",
+  players: ["John", "Pete", "Alice"],
+
+  showList() {
+    this.players.forEach(
+      function (player) {
+        alert(this.title + ": " + player);
+      }.bind(this)
+    );
+  }
+};
+
+group.showList();
+*/
+
+/*
+let group = {
+  title: "Our Players",
+  players: ["John", "Pete", "Alice"],
+
+  showList() {
+    this.players.forEach((player) => {
+      alert(this.title + ": " + player);
+    });
+  }
+};
+
+
+group.showList();
+*/
