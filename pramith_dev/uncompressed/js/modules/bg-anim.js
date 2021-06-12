60bac4bfdda71a9819fffebbf5fa03a18378d6fc
Pramith Prakash <pprakash2@sapient.com>
2021-06-12 08:23:56 +0530

    color change

--- pramith_dev/uncompressed/js/modules/bg-anim.js
+++ pramith_dev/uncompressed/js/modules/bg-anim.js
@@ -153,7 +153,7 @@
             ctx.beginPath();
             ctx.moveTo(p.x, p.y);
             ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(255,81,47,'+ p.active+')';
+            ctx.strokeStyle = 'rgba(43,0,212,'+ p.active+')';
             ctx.stroke();
         }
     }
@@ -172,7 +172,7 @@
             if(!_this.active) return;
             ctx.beginPath();
             ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(255,81,47,'+ _this.active+')';
+            ctx.fillStyle = 'rgba(43,0,212,'+ _this.active+')';
             ctx.fill();
         };
     }
